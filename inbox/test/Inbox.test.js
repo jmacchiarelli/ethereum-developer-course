4bfc2b292b8b635a30ef36a9f17eef537223711e
Jennifer Macchiarelli <jenmacdev@gmail.com>
2018-02-04 14:42:26 -0500

    Section 2, Lesson 37, setting up test infrastructure by installing and requiring modules

---
+++ inbox/test/Inbox.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+// assert module, standard library in node js runtime
+// making assertions about tests
+
+const ganache = require('ganache-cli');
+// local ethereum test network
+
+const Web3 = require('web3');
+// make use of Web3 importing a constructor function
+// used to create intstances of the Web3 library
+// use capital for this one
+
+
+// instance of Web3, assign the instance to a variable that is lowercase
