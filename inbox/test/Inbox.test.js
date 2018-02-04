f3193746f06d6d07b2fe90106d7f8123c2a8bf4e
Jennifer Macchiarelli <jenmacdev@gmail.com>
2018-02-04 15:17:48 -0500

    Section 2, Lesson 39, setup local instance of web3

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -8,7 +8,23 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 // make use of Web3 importing a constructor function
 // used to create intstances of the Web3 library
-// use capital for this one
-
-
 // instance of Web3, assign the instance to a variable that is lowercase
+
+// Web3 versioning, in ethereum end all solution for communication
+// programmatic access to ethereum network
+// 2 groups in the wild, v0.x.x and v1.x.x
+// We installed v1, vast majority of things are using older v
+// Thanks Stephen
+// v0 primitive interface, only callback functions for async code
+// v1 support for promises, async/wait
+// Stephen says no callbacks, no spaghettti code
+
+// How Web3 is organized
+// Use constructor to make an instance of web3
+// Have to set up a provider to comm between web3 and ethereum network
+
+const web3 = new Web3 (ganache.provider());
+// Setup local isntance of web3
+// Tells instance to connect to provider
+// Will change over time depending on what network you connect to
+// This comes up again with metamask
