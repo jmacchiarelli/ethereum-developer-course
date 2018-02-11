9d12a0462b0dcf286dcd5a0a34e529826d9a409d
Jennifer Macchiarelli <jenmacdev@gmail.com>
2018-02-11 15:02:11 -0500

    adding helper function pseudo random number generator

--- lottery/lottery.sol
+++ lottery/lottery.sol
@@ -12,8 +12,16 @@ contract Lottery {
 // Require is for validation before code inside the function executes
 // For example you can require a value aka min eth to send in wei
     function enter() public payable {
-        require(msg.value > .01 ether );
+        require(msg.value > .01 ether);
 
         players.push(msg.sender);
+    }
+
+// Helper function, pseudo random number generator, can be gamed
+// Take current block difficulty, current time, addresses of players
+// Feed them in to a SHA3 algo, spits out a really big number
+// Use really big number to pick random winner
+    function random() private view returns (uint) {
+        return uint(keccak256(block.difficulty, now, players));
     }
 }
