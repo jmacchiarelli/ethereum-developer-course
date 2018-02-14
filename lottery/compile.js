88cad0650ccb2bc8a8a4cb6f9c0559481246764e
Jennifer Macchiarelli <jenmacdev@gmail.com>
2018-02-14 08:31:52 -0500

    lottery test setup

--- lottery/compile.js
+++ lottery/compile.js
@@ -1,34 +1,8 @@
 const path = require('path');
-// helps us build path over to the inbox sol file
-// using path for this to guarantee cross platform compatibility
-// if windows or unix will generate a valid path
-
 const fs = require('fs');
-// file system
-
 const solc = require('solc');
-// require solc
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-// generate a path to Inbox sol file
-// pass in 3 portions of the path
-// dirname here = inbox dir
-// inbox/contracts/Inbox.sol
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
+const source = fs.readFileSync(lotteryPath, 'utf8');
 
-const source = fs.readFileSync(inboxPath, 'utf8');
-// read in the contents of the file
-// pass in inbox path and specify encoding
-
-
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
-// pass in source code
-// specify number of contracts we are trying to compile
-// wrap compile call in a console log call
-// log the output to tell us what the compiler is doing
-// when you run in console: node compile.js
-// log outputs properties: bytecode, interface / contract abi
-// had us remove console log
-// now going to export this big giant object
-// just return details about inbox contract
-// adding contracts property, key value pair of inbox
-// makes it easier to test and deploy
+module.exports = solc.compile(source, 1).contracts[':Lottery'];
--- lottery/deploy.js
+++ lottery/deploy.js
@@ -2,7 +2,6 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
-// Creating a path to retrieve the mnemonic pass phrase
 const path = require('path');
 const fs = require('fs');
 
@@ -10,9 +9,6 @@ const secretsPath = path.resolve(__dirname, '../../secrets', 'metamask-acct1-ude
 
 const secretWords = fs.readFileSync(secretsPath, 'utf8');
 
-// Which account we want to unlock
-// Specify what outside node or api we want to connect to
-// Add account mnemonic, but instead its been hidden in a local dir
 const provider = new HDWalletProvider(
   secretWords,
   'https://rinkeby.infura.io/8C5763b17wcbkLRZ5BFB'
@@ -20,19 +16,13 @@ const provider = new HDWalletProvider(
 
 const web3 = new Web3(provider);
 
-// Two pieces of async code cannot use await outside of a function
-// Writing a function to use async syntax
-// Get a list of accounts unlocked
-// Console log to specify which acct to deploy from
-// Deploy dot specifies what to send
-// Send dot specifies how much gas, which acct
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
   console.log('Attempting to deploy from account', accounts[0] );
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .deploy({ data: bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
