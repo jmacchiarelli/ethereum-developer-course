e54fdaf2e70e0c6a8e3e9ce32499b229579619b5
Jennifer Macchiarelli <jenmacdev@gmail.com>
2018-02-16 16:27:16 -0500

    adding managed by address, number of players entered, balance of contract

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -5,22 +5,32 @@ import web3 from './web3';
 import lottery from './lottery';
 
 // State is taking place inside the constructor
+// Manager is an empty string, players is an empty array
 class App extends Component {
   state = {
-    manager: ''
+    manager: '',
+    players: [],
+    balance: ''
   };
 
+// Get contract manager
+// Get list of players
+// Print the balance of the lottery using contract address
   async componentDidMount(){
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({ manager });
+    this.setState({ manager, players, balance });
   }
 
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>This contract is managed by {this.state.manager}.
+        There are currently {this.state.players.length} people entered competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+        </p>
       </div>
     );
   }
