fc93e02ec627773fc261583af11b6099c0d851f9
Jennifer Macchiarelli <jenmacdev@gmail.com>
2018-02-16 15:18:55 -0500

    add address and abi from deployed lottery contract

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -5,7 +5,7 @@ import web3 from './web3';
 
 class App extends Component {
   render() {
-    web3.eth.getAccounts().then(console.log);
+//    web3.eth.getAccounts().then(console.log);
 //    console.log(web3.version);
 
     return (
