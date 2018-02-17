8020c2f55c048eacff5c01500866888bb58b7d59
Jennifer Macchiarelli <jenmacdev@gmail.com>
2018-02-16 21:35:57 -0500

    adding pick a winner

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -40,6 +40,17 @@ class App extends Component {
     this.setState({ message: 'You have been entered' });
   };
 
+  onClick = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction success...' })
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    this.setState({ message: 'A winner has been picked!' })
+  };
 
   render() {
     return (
@@ -48,7 +59,6 @@ class App extends Component {
         <p>This contract is managed by {this.state.manager}.
         There are currently {this.state.players.length} people entered competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
         </p>
-
         <hr />
 
         <form onSubmit={this.onSubmit}>
@@ -64,6 +74,12 @@ class App extends Component {
         </form>
 
         <hr />
+
+        <h4>Ready to pick a winner?</h4>
+        <button onClick={this.onClick}>Pick a winner!</button>
+
+        <hr />
+
         <h1>{this.state.message}</h1>
       </div>
     );
